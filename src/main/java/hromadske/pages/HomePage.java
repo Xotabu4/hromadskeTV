cb39b4d8566e13d777fa7602e6dbca4ce6ac9fc3
Oleksandr Khotemskyi <xotabu4@gmail.com>
2015-03-28 16:47:22 +0400

    fixing failures with small screen on travis

--- src/main/java/hromadske/pages/HomePage.java
+++ src/main/java/hromadske/pages/HomePage.java
@@ -2,6 +2,7 @@ package hromadske.pages;
 
 import com.codeborne.selenide.Selenide;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 /*
@@ -12,6 +13,11 @@ import static com.codeborne.selenide.Selenide.$;
 public class HomePage extends Page {
 
     public SearchPage searchFor(String text) {
+        //When screen is too small - search field is hided - need to click on opener first.
+        if ($("span.btn-opener1").isDisplayed()) {
+            $("span.btn-opener1").click();
+            $("#text-01").waitUntil(visible, 1000);
+        }
         type($("#text-01"), text);
         //Clicking search button.
         $("input[value='search']").click();
